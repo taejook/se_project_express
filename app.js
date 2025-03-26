88a5fdcf444c09c82ffe7940963c445902925e26
TaeJoo Kim <taejook1107@gmail.com>
2025-03-26 19:50:58 -0400

    added controllers, models and routes file. created schemas for user and clothing items. Also created routes for the users

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const mainRouter = require("./routes/index");
 
 const app = express();
-
 const { PORT = 3001 } = process.env;
+
+mongoose
+.connect('mongodb://127.0.0.1:27017/wtwr_db')
+.then (() => {
+    console.log("Connected to DB");
+})
+.catch(console.error);
+
+app.use("/", mainRouter);
 
 app.listen(PORT, ()=> {
     console.log(`Listening on ${PORT}`);
---
+++ models/clothingItem.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const clothingItemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 30
+    },
+
+    weather: {
+        type: String,
+        required: true,
+        enum: ['hot', 'warm', 'cold'],
+    },
+
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: {
+            validator(value) {
+              return validator.isURL(value);
+            },
+            message: 'You must enter a valid URL',
+          }
+    },
+
+    owner: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'user'
+    },
+
+    likes: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        default: []
+    },
+
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('clothingItem', clothingItemSchema),
