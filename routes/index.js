c762c01904c5f03d52ff876b0ca4ae465b89d983
TaeJoo Kim <taejook1107@gmail.com>
2025-04-15 18:16:37 -0400

    fix: added constant for the 404 status code in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,13 @@ const router = require("express").Router();
 
 const userRouter = require("./users");
 const clothingItems = require("./clothingItems");
+const { NOT_FOUND_STATUS_CODE } = require("../utils/errors");
 
 router.use("/items", clothingItems);
 router.use("/users", userRouter);
 
 router.use((req, res) => {
-  res.status(404).send({ message: "Requested resource not found" });
+  res.status(NOT_FOUND_STATUS_CODE).send({ message: "Requested resource not found" });
 });
 
 
